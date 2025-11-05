09aa94911fc959bc1ae5dae007f5605422193462
cheynskjento <cheynskjent@gmail.com>
2025-11-05 10:40:37 +0100

    Setup basic navigation: Tabs (Home + Profile) and Home Stack Navigator

---
+++ navigation/MainNavigator.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import DetailScreen from '../screens/DetailScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="HomeList" component={HomeScreen} options={{ title: 'Pokédex' }} />
+      <Stack.Screen name="Detail" component={DetailScreen} options={{ title: 'Pokémon Details' }} />
+    </Stack.Navigator>
+  );
+}
+
+export default function MainNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeStack} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
